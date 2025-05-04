19eb8143c425dfcc284d88f184af604b8ff55c4f
Ibrahima Toure <145729747+IbrahimaST@users.noreply.github.com>
2025-05-03 20:16:22 -0400

    removed unnecessary imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import DuaCard from './components/DuaCard';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import SpeakHello from './practice/SpeakHello';
-import SpeakInput from './practice/SpeakInput';
 
 function App() {
   return (
